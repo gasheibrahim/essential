fb1377c8405daadbd222e6f92e491caf68dc220c
gasheibrahim <ibrahimgashema154@gmail.com>
2021-09-08 16:57:54 +0200

    search correction

--- assets/js/thisess.js
+++ assets/js/thisess.js
@@ -1,31 +1,42 @@
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 4,
-  spaceBetween: 0,
-  //   loop: true,
-  arrow: true,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
-  breakpoints: {
-    200: {
-      slidesPerView: 1,
+    slidesPerView: 4,
+    spaceBetween: 0,
+    //   loop: true,
+    arrow: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
-    300: {
-      slidesPerView: 1,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
     },
-    600: {
-      slidesPerView: 1,
+    breakpoints: {
+        200: {
+            slidesPerView: 1,
+        },
+        300: {
+            slidesPerView: 1,
+        },
+        600: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 1,
+        },
+        1024: {
+            slidesPerView: 4,
+        },
     },
-    768: {
-      slidesPerView: 1,
-    },
-    1024: {
-      slidesPerView: 4,
-    },
-  },
 });
+const showForm = (toggleId) => {
+    const toggle = document.getElementById(toggleId),
+        searchForm = document.getElementById('form');
+
+    if (toggle) {
+        toggle.addEventListener('click', () => {
+            searchForm.classList.toggle("none");
+        })
+    }
+}
+showForm('form-search')
